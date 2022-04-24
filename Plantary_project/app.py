db1890e29f26fd3e26ca81a20af38f470ed6abe8
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-04-24 23:25:44 +0530

    Update app.py

--- Plantary_project/app.py
+++ Plantary_project/app.py
@@ -35,4 +35,18 @@ It will auto restart on change so be aware.
 @app.route("/Super_simple2")
 def super_simple2():
     return jsonify(message = "Hello from Planetary API",id = 2)
+"""
+jsonify is helpfull to create json file as shown as above example.
+"""
 
+
+"""
+All web apps are based on a request-response mechanism
+
+Requests and responses have headers which is metadata.
+headers is characteristics of the request or response.
+
+like Status code which tell you that request was successful or not.
+
+
+"""
