0f70aed49a50bf7cde60a9794e3a7131d5b18f61
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-05-01 20:26:23 +0530

    JWT config

--- Plantary_project/app.py
+++ Plantary_project/app.py
@@ -4,14 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String , Float
 import os
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager,jwt_manager,jwt_required,create_access_token,create_refresh_token
+
 
 app = Flask(__name__)
 #app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'planets.db')
+app.config["JWT_SECRET_KEY"] = 'unique_key'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
+
+
 
 
 """
