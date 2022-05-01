7e8fbed7671e18741cdf5ab3bc635fad01563918
meetshahcode <72691054+meetshahcode@users.noreply.github.com>
2022-05-01 20:36:55 +0530

    login complete

--- Plantary_project/app.py
+++ Plantary_project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request, url_for
+from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String , Float
 import os
